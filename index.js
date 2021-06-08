aaad0f43aa492d35f729b3dc117239bb4ea9b3cb
Antonio <tenlla2@gmail.com>
2021-06-08 23:52:47 +0200

    changed port server

--- index.js
+++ index.js
@@ -7,7 +7,4 @@ const port = process.env.port || 3000;
 // Routes of the project
 app.use(require("./routes"));
 
-app.listen(port, () => {
-  console.log(`Example app listening at port ${port}`);
-});
-
+app.listen(process.env.PORT || 5000);
